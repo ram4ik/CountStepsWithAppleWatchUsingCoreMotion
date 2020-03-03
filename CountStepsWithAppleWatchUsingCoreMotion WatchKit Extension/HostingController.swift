832da0dde4f138e85e0733aaa12106b5cd3f6ff9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2020-03-03 15:38:05 +0200

    Initial Commit

---
+++ CountStepsWithAppleWatchUsingCoreMotion WatchKit Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  CountStepsWithAppleWatchUsingCoreMotion WatchKit Extension
+//
+//  Created by ramil on 03.03.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ CountStepsWithAppleWatchUsingCoreMotion WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  CountStepsWithAppleWatchUsingCoreMotion WatchKit Extension
+//
+//  Created by ramil on 03.03.2020.
+//  Copyright © 2020 com.ri. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
